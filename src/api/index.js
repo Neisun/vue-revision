bf2af085dcfe8d21cc9dff99918fb4b60f715de4
hns <hunaisong@163.com>
2017-08-14 17:38:38 +0800

    稍微修改

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,9 @@
  * @Author: hunaisong 
  * @Date: 2017-08-11 10:25:58 
  * @Last Modified by: hunaisong
- * @Last Modified time: 2017-08-11 13:54:45
+ * @Last Modified time: 2017-08-14 09:26:32
  */
 export const myTest = function () {
      console.log("test")
 }
-
+module.exports = api
